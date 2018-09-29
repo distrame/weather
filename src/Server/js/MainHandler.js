36a44b3f90dc86780dfc6d6b574fcd1e03ab813d
Ivan Bandorin <bandorin.ivan@mail.ru>
2018-09-29 19:43:07 +0300

    kurwa

--- src/Server/js/MainHandler.js
+++ src/Server/js/MainHandler.js
@@ -24,7 +24,7 @@ function onlod() {
             "class": "d-flex justify-content-center"
         }).appendTo("#weather_current");
         {
-            $("<h5> " + rej.current.location.name + " </h5>", {
+            $("<h4> " + rej.current.location.name + " </h4>", {
                 "id": "weather_current-" + "title"
             }).appendTo("#weather_current-head");
         }
@@ -37,9 +37,44 @@ function onlod() {
             $("<img src=" + rej.current.current.condition.icon + "/>", {
                 "id": "weather_current-" + "icon"
             }).appendTo("#weather_current-body");
-            $("<span> " + rej.current.current.condition.text + " </span>", {
-                "id": "weather_current-" + "state"
+            $("<div/>", {
+                "id": "weather_current-" + "info"
             }).appendTo("#weather_current-body");
+            {
+                $("<span> " + rej.current.current.condition.text + " </span>", {
+                    "id": "weather_current-" + "state"
+                }).appendTo("#weather_current-info");
+                $("<span> " + rej.current.current.temp_c + " по цельсию. </span>", {
+                    "id": "weather_current-" + "temp"
+                }).appendTo("#weather_current-info");
+            }
         }
     }
+
+    for (var i = 0; i < 7; i++) {
+        $("<div/>", {
+            "id": "weather_day_" + i + "-" + "body",
+            "class": "d-flex justify-content-center"
+        }).appendTo("#weather_day_" + i + "");
+        {
+            $("<img src=" + rej.forecast.forecastday[i].condition.icon + "/>", {
+                "id": "weather_day_" + i + "-" + "icon"
+            }).appendTo("#weather_day_" + i + "-body");
+            $("<div/>", {
+                "id": "weather_day_" + i + "-" + "info"
+            }).appendTo("#weather_day_" + i + "-body");
+            {
+                $("<span> " + rej.forecast.forecastday[i].condition.text + " </span>", {
+                    "id": "weather_day_" + i + "-" + "state"
+                }).appendTo("#weather_day_" + i + "-info");
+                $("<span> Максимальная " + rej.forecast.forecastday[i].maxtemp_c + "C </span>", {
+                    "id": "weather_day_" + i + "-" + "temp_max"
+                }).appendTo("#weather_day_" + i + "-info");
+                $("<span> Минимальная " + rej.forecast.forecastday[i].mintemp_c + "C </span>", {
+                    "id": "weather_day_" + i + "-" + "temp_min"
+                }).appendTo("#weather_day_" + i + "-info");
+            }
+        }
+    }
+
 }
