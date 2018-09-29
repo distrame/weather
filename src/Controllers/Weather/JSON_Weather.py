5e4ce1c7f4f53d46414abb8f3f4fe1a1544d4c7c
Ivan Bandorin <bandorin.ivan@mail.ru>
2018-09-29 17:25:15 +0300

    microfix

--- src/Controllers/Weather/JSON_Weather.py
+++ src/Controllers/Weather/JSON_Weather.py
@@ -18,12 +18,18 @@ class WeatherJSONBy_user_ip(BaseHandler):
     def post(self):
         ip = (HTTPHeaders.parse(str(self.request.headers))).get("X-Real-Ip")
 
-        res = requests.get(url=get_weather_api_url() + "/current.json",
-                           data={"key": get_weather_api_key(),
-                                 "q": ip,
-                                 "days": 7,
-                                 "lang": "ru"})
-        print(res.url)
+        url = get_weather_api_url() + "/current.json" + "?"
+        dict_ = {"key": get_weather_api_key(),
+                 "q": ip,
+                 "days": 7,
+                 "lang": "ru"}
+
+        for i in dict_:
+            url += "{}={}&".format(i, dict_[i])
+        url = url[:-1]
+
+        res = requests.get(url=url)
         rej = res.json()
+        
         self.write(rej)
         return
