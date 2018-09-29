a4ae8eb6a29d149cfe48999f35be0c0e1a8de4a0
Ivan Bandorin <bandorin.ivan@mail.ru>
2018-09-29 15:44:16 +0300

    -

--- src/Controllers/Weather/IP.py
+++ src/Controllers/Weather/IP.py
@@ -7,11 +7,11 @@ class IP(BaseHandler):
     """
 
     def get(self):
-        self.write({"request": str(self.request),
-                    "arguments": self.request.arguments,
-                    "body": self.request.body,
-                    "full_uri": self.request.full_uri,
-                    "ip": self.request.remote_ip,
-                    "headers": self.request.headers})
         print(dir(self.request))
+        self.write({"request": str(self.request),
+                    "arguments": str(self.request.arguments),
+                    "body": str(self.request.body),
+                    "full_uri": str(self.request.full_uri),
+                    "ip": str(self.request.remote_ip),
+                    "headers": str(self.request.headers)})
         return
